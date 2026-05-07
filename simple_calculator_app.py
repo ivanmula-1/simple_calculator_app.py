28d39437843b19d16299caa86a19daba1bac2e10
ivanmula <ivanmula74@gmail.com>
2026-05-08 00:11:40 +0800

    Initial Commit

---
+++ simple_calculator_app.py
@@ -0,0 +1,11 @@
+def calculate(first_number, second_number, operation):
+    if operation == 1:
+        return first_number + second_number
+    elif operation == 2:
+        return first_number - second_number
+    elif operation == 3:
+        return first_number * second_number
+    elif operation == 4:
+        if second_number == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
+        return first_number / second_number
